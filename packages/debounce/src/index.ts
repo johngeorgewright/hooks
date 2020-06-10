85b4e6a4fa1801803bb8c1c57d1c6abaf8a4df42
John Wright <johngeorge.wright@gmail.com>
2020-06-10 15:14:28 +0100

    refactor(debounce): further refactor

--- packages/debounce/src/index.ts
+++ packages/debounce/src/index.ts
@@ -1,21 +1,16 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function useDebounce<Value>(value: Value, delay: number): Value {
   const [debouncedValue, setDebouncedValue] = useState(value)
-
-  const onTimeout = useCallback(() => {
-    setDebouncedValue(value)
-  }, [value])
 
   useEffect(() => {
     if (value === debouncedValue) {
       return undefined
     }
 
-    const timeout = setTimeout(onTimeout, delay)
-
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
     return () => clearTimeout(timeout)
-  }, [delay, debouncedValue, onTimeout, value])
+  }, [delay, debouncedValue, value])
 
   return debouncedValue
 }
